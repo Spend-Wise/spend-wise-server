58857dd7e48527d0c5dad5897fe7baa9ddac9d27
Aman <83913651+Aman-shabbas@users.noreply.github.com>
2025-11-12 01:12:08 +0530

    [#19] [Aman] Adds schema for db implementation

---
+++ app/db/schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+
+class UserRead(BaseModel):
+    id: int
+    username: str
+    password: str
+
+    class Config:
+        orm_mode = True
