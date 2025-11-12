57949989928537af7e094ab23a88e0781b0de303
sujoy <sujoychoolur@gmail.com>
2025-11-12 22:00:36 +0530

    [#18] [sujoy] updated the mock repository layer for storing user details

--- app/db/repository/user.py
+++ app/db/repository/user.py
@@ -1,25 +1,27 @@
 import uuid
+
 from ..schema import UserCreate, UserRead
 
-users = [];
+users = []
 
-def save_user(userCreate: UserCreate) -> UserRead:
+
+def save_user(user_create: UserCreate) -> UserRead:
     user = UserRead(
-        id=str(uuid.uuid4()),
-        username=userCreate.username,
-        password=userCreate.password
+        id=str(uuid.uuid4()), username=user_create.username, password=user_create.password
     )
     users.append(user)
     return user
-    
+
+
 def get_user_by_id(user_id: str) -> UserRead | None:
     for user in users:
         if user["id"] == user_id:
             return user
-    return None;
+    return None
+
 
 def get_user_by_name(username: str) -> UserRead | None:
     for user in users:
         if user["username"] == username:
             return user
-    return None;
+    return None
--- app/db/schema.py
+++ app/db/schema.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 
+
 class UserCreate(BaseModel):
     username: str
     password: str
+
 
 class UserRead(BaseModel):
     id: str
